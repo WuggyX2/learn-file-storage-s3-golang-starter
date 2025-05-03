7c86892f78084827c27650d41c09ae5c8102d881
WuggyX2 <matti.virkkunen@ceili.fi>
2025-05-03 20:41:06 +0300

    Finish chapter 1

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +33,88 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+
+	fileExtension, err := getFileExtensionFromMime(mediaType)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Uploaded file is not an image", err)
+		return
+	}
+
+	filePath := filepath.Join(cfg.assetsRoot, videoIDString+fileExtension)
+
+	fileOnDisk, err := os.Create(filePath)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to save a file", err)
+		return
+	}
+
+	newUrl := fmt.Sprintf("http://localhost:%s/%s", cfg.port, filePath)
+
+	_, err = io.Copy(fileOnDisk, file)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read file to disk", err)
+		return
+	}
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+
+	if err != nil {
+		respondWithError(
+			w,
+			http.StatusInternalServerError,
+			"Unable to retrieve video metadata",
+			err,
+		)
+		return
+	}
+
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized error", err)
+		return
+	}
+
+	videoMetadata.ThumbnailURL = &newUrl
+	err = cfg.db.UpdateVideo(videoMetadata)
+
+	if err != nil {
+		respondWithError(
+			w,
+			http.StatusInternalServerError,
+			"Error updating video data",
+			err,
+		)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoMetadata)
+}
+
+func getFileExtensionFromMime(mimeType string) (string, error) {
+
+	const mimePrefix = "image/"
+
+	if !strings.HasPrefix(mimeType, mimePrefix) {
+		return "", errors.New("Given file type is not an image")
+	}
+
+	result := strings.Split(mimeType, mimePrefix)
+
+	return "." + result[1], nil
+
 }
